1bd940c5bacfe2f6ea46e06330b0c08bb0dce6e2
JoaoRobert0 <chavescamboim2004@gmail.com>
2024-02-20 22:05:15 -0300

    Capitulo 1 finalizado

---
+++ Cap_1/Exercicios/1.2.py
@@ -0,0 +1,12 @@
+# 1.2 – Erros de digitação em Hello World: Abra o arquivo hello_world.py que
+# você acabou de criar. Introduza um erro de digitação em algum ponto da linha e
+# execute o programa novamente. Você é capaz de inserir um erro de digitação que
+# gere um erro? Consegue entender a mensagem de erro? É capaz de inserir um erro
+# de digitação que não produza um erro? Por que você acha que isso não gerou um
+# erro?
+
+# Ocorrera um erro devido usar uma aspa simples com uma dupla
+# print('Hello Python World!")
+
+# A mensagem estar gramaticalmente errada porem não é um erro de sintaxe
+print("H3ll0 World") 
---
+++ Cap_1/Exercicios/1.3.py
@@ -0,0 +1,11 @@
+# 1.3 – Habilidades infinitas: Se tivesse infinitas habilidades de programação, o que
+# você desenvolveria? Você está prestes a aprender a programar. Se tiver um
+# objetivo final em mente, você terá um uso imediato para suas novas habilidades;
+# agora é um ótimo momento para esboçar descrições do que você gostaria de criar.
+# É um bom hábito manter um caderno com “ideias” que você possa consultar sempre
+# que quiser iniciar um novo projeto. Reserve alguns minutos para descrever três
+# programas que você gostaria de criar.
+
+# Desenvolver um app Pomodoro
+# Desenvolver um sistema de controle de comandas de restaurantes
+# Desenvolver algum jogo com Pygame
