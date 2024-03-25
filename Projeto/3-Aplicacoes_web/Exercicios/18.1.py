37eb687a75b9ae4a4796e263ad34c8d206b3b37b
João Roberto Chaves Camboim <20231014040011@ifrn.local>
2024-03-25 11:56:30 -0300

    Iniciando projeto django

---
+++ Projeto/3-Aplicacoes_web/Exercicios/18.1.py
@@ -0,0 +1,10 @@
+# 18.1 – Novos projetos: Para ter uma ideia melhor do que Django faz, crie dois
+# projetos vazios e observe o que o framework criou. Crie uma nova pasta com um
+# nome  simples,  por  exemplo,  InstaBook  ou  FaceGram  (fora  de  seu  diretório
+# learning_log), navegue até essa pasta em um terminal e crie um ambiente virtual.
+# Instale Django e execute o comando django-admin.py startproject instabook
+# . (lembre-se de incluir o ponto no final do comando).
+# Observe os arquivos e as pastas criados por esse comando e compare-os com os
+# de Learning Log. Faça isso algumas vezes até ter familiaridade com o que Django
+# cria quando um novo projeto é iniciado. Então apague os diretórios dos projetos se
+# quiser
